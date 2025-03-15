6e34073faf8cdd002ad0a7d0a4f1ab36472d5802
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-15 17:32:36 +0530

    Automated commit: frontend/src/components/Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2023 Tic-Tac-Toe Game. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
